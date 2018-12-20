30b17c443c5c3b195d7519b227ce29300b1f5bb9
danielkansaon <danielkansaon>
2018-12-20 14:06:18 -0200

    create

---
+++ src/main/java/com/javaee/sistema_acoes/SistemaAcoesApplication.java
@@ -0,0 +1,14 @@
+package com.javaee.sistema_acoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcoesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/javaee/sistema_acoes/SistemaAcoesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.javaee.sistema_acoes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaAcoesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
