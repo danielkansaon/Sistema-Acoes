649f90467d035b166e2fbf72e08ebd947e233644
danielkansaon <danielkansaon>
2018-12-21 22:44:10 -0200

    atualizando

--- src/main/java/com/javaee/sistema_acoes/repositories/IAcaoRepository.java
+++ src/main/java/com/javaee/sistema_acoes/repositories/IAcaoRepository.java
@@ -9,8 +9,4 @@ import com.javaee.sistema_acoes.domain.Acao;
 
 @Repository
 public interface IAcaoRepository extends MongoRepository<Acao, Long> {
-
-	public Acao obter_acao_empresa(long idEmpresa);
-	public Acao comprar_acao(long idCliente, long idAcao, Date dataCompra);
-	public Acao vender_acao(long idCliente, long idAcao);
 }
